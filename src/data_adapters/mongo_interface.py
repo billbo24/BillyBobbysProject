99640625f5595e9cf1ff5ee26cf85a6907967503
billbo24 <wfloyd1231@gmail.com>
2023-11-18 23:20:27 -0500

    Removed Push to mongo function

--- src/data_adapters/mongo_interface.py
+++ src/data_adapters/mongo_interface.py
@@ -27,20 +27,4 @@ def get_supply_info(client: MongoClient):
     print(finalData[0])
 
 
-def push_to_mongo(client: MongoClient,data_to_push,database,collection):
-    #This will take JSON data and push it to mongo
-    #I suppose if something isn't JSON then we should break it
-    #As best I can tell JSON is really just...a dictionary???
-    if isinstance(data_to_push,dict) == False:
-        #Not a dictionary...no dice
-        print("Not JSON data, beat it loser")
-        return False
-    
-    #Alright, after some playing around it seems like the fundamental
-    #unit of data here are dictionaries, but referred to as documents.  
-    #A standard python array containing all the dictionaries we want
-    #to push will allow us to use the col.insert_many() function
-    
-    
-    return False
 
